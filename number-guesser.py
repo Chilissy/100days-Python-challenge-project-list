921efe8f2071ec5d5f9f075f01e1757736531f68
Alex Chilissy <Chilissy@users.noreply.github.com>
2023-03-31 11:57:14 +0200

    Added comments
    Wrote explanatory DocStrings on what the code does.

--- number-guesser.py
+++ number-guesser.py
@@ -1,37 +1,38 @@
 #Number Guessing Game Objectives:
 import random
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
-RANDOM_NUMBER = random.randint(0,100)
-EASY = 10
-HARD = 5
+
+random_nr = random.randint(0,100)
+easy = 10
+hard = 5
+
+
+""" check() method contains the user_input variable and changes it to int. It also contains number() method with status parameter. 
+number() method compares the user_input with randomly generated number and returns the status of the equation."""
 
 def check():
-    user_input = input("What numbah?")
+    user_input = input("What number?")
     user_input = int(user_input)
     def number(status):
-        if user_input == RANDOM_NUMBER:
+        if user_input == random_nr:
             print("Great!")
             quit()
-        elif user_input < RANDOM_NUMBER:
+        elif user_input < random_nr:
             status = "Too low"
             return status
         else:
             status = "Too high"
             return status
     print(number(user_input))
+    
+    
+""" Code below allow player to choose the difficulty. Playing on hard difficulty gives player 5 chances to guess the correct number and easy 10."""
 
 z = input("Hey, let's play guess the number game. Do you want the difficulty to be easy or hard?")
 
 if z == "easy":
-    for i in range (0, EASY):
+    for i in range (0, easy):
         check()
 else:
-    for i in range (0, HARD):
+    for i in range (0, hard):
         check()
