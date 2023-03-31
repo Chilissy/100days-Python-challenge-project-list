cf6b63e637c84f6405b4c167a5fe7319796f0ef3
Alex Chilissy <Chilissy@users.noreply.github.com>
2023-03-31 13:32:03 +0200

    Added comments

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -1,9 +1,11 @@
+""" BELOW: Imported all necessary modules to make the system work."""
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
 
+""" BELOW: Iterating throgh the bank of question (data.py). """
 for obj in range (0, len(question_data)):
     x = question_data[obj]["question"]
     y = question_data[obj]["correct_answer"]
@@ -12,6 +14,7 @@ for obj in range (0, len(question_data)):
 
 quiz = QuizBrain(question_bank)
 
+""" BELOW: while loop that keeps the question coming."""
 while quiz.still_has_question() == True:
     quiz.next_question()
 
