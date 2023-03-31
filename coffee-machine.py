718ccf9e107ef85c83c2b140d7842ca7bcfc6780
Alex Chilissy <Chilissy@users.noreply.github.com>
2023-03-31 13:20:10 +0200

    Comments + check_milk(espresso) + shortened + rounded
    Check_milk function checked for milk ingredients in espresso. Espresso doesn't contain variable milk.
    
    110-LINE
    if drink != "espresso":
        check_milk(drink)

--- coffee-machine.py
+++ coffee-machine.py
@@ -36,22 +36,16 @@ DIMES_VALUE = 0.1
 NICKLES_VALUE = 0.05
 PENNIES_VALUE = 0.01
 
-############################################################################################
-############################################################################################
-###########This code can be improved, but I am not sure how. LF Tutor to analyze!###########
-############################################################################################
-############################################################################################
-
-
+""" BELOW: Function that checks the current amount of resources."""
 def report(drink):
     print(f"water: {resources['water']}\nmilk: {resources['milk']}\ncoffee: {resources['coffee']}\nmoney: {resources['money']}")
 
-
+""" BELOW: Function that takes money from user, check if there's enough resources. Then it counts if there's enough money, and if so, returns error or a coffee."""
 def order(drink):
     if drink == "report":
         report(drink)
         return order(str(input("What would you like? (espresso/latte/cappuccino)\n")))
-    if drink == "espresso":
+    if drink == "latte" or drink == "cappuccino" or drink == "espresso":
         check_resources(drink)
         in_total = float(round(payment(), 2))
         if in_total < MENU[drink]['cost']:
@@ -64,59 +58,44 @@ def order(drink):
         else:
                 rest = in_total - MENU[drink]['cost']
                 round(rest, 2)
-                use_resources(drink)
-                print(f"Here's your {drink}. Your rest: ${rest}")
-                return round(in_total, 2)
-    elif drink == "latte" or drink == "cappuccino":
-        check_resources(drink)
-        in_total = float(round(payment(), 2))
-        if in_total < MENU[drink]['cost']:
-                print("Not enough money. Money refunded.")
-                return order(str(input("What would you like? (espresso/latte/cappuccino)\n")))
-        elif in_total == MENU[drink]['cost']:
-                print(f"Here's your {drink}, have a nice day!")
-                use_resources(drink)
-                return round(in_total, 2)
-        else:
-                rest = in_total - MENU[drink]['cost']
-                round(rest, 2)
-                print(f"Here's your {drink}. Your rest: ${rest}")
+                print(f"Here's your {drink}. Your rest: ${round(rest, 2)}")
                 use_resources(drink)
                 return round(in_total, 2)
 
-
+""" BELOW: Function that checks resources. If there's no enough resources, ask user for refill."""
 def check_resources(drink):
     def check_water(drink):
         if resources['water'] < MENU[drink]['ingredients']['water']:
             print("No water.")
-            if input("Refill?") == "yes":
-                refill()
+            print("...")
+            refill()
             return True
     def check_coffee(drink):
         if resources['coffee'] < MENU[drink]['ingredients']['coffee']:
             print("No coffee.")
-            if input("Refill?") == "yes":
-                refill()
+            print("...")
+            refill()
             return True
     def check_milk(drink):
         if resources['milk'] < MENU[drink]['ingredients']['milk']:
             print("No milk.")
-            if input("Refill?") == "yes":
-                refill()
+            print("...")
+            refill()
             return True
     check_water(drink)
     check_coffee(drink)
-    check_milk(drink)
+    if drink != "espresso":
+        check_milk(drink)
     return True
 
-
+""" BELOW: Function that refill resources."""
 def refill():
     resources['water'] = 300
     resources['milk'] = 200
     resources['coffee'] = 100
     print("Ingredients refilled successfully.")
 
-
+""" BELOW: Function that takes the money and multiply by their value. Then return total value"""
 def payment():
     quarters = float(input("Quarters: "))
     dimes = float(input("Dimes: "))
@@ -129,7 +108,7 @@ def payment():
     in_total = quarters_sum + dimes_sum + nickles_sum + pennies_sum
     return in_total
 
-
+""" BELOW: Function that uses ingredients according to chosen type of coffee. It takes needed resources away."""
 def use_resources(drink):
     resources['water'] -= MENU[drink]['ingredients']['water']
     resources['coffee'] -= MENU[drink]['ingredients']['coffee']
@@ -140,7 +119,7 @@ def use_resources(drink):
     return money_status
 
 
-
+""" ENGINE"""
 while True == True:
     USER_INPUT = str(input("What would you like? (espresso/latte/cappuccino)\n"))
     drink = USER_INPUT
